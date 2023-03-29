650543fdbe6b4cf636003e9dcfd4a17048662a21
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2023-03-29 16:13:34 +0400

    Add files via upload

---
+++ JS-BAS-TASK/js/index.js
@@ -0,0 +1,16 @@
+
+// Ürünleri temsil eden bir dizi oluşturun
+// let products = [
+//   { id: 1, name: 'Harry Poter', price: 20 },
+//   { id: 2, name: 'Harry Poter 2', price: 30 },
+//   { id: 3, name: 'Harry Poter 3', price: 40 },
+//   { id: 4, name: 'Harry Poter 4', price: 50 },
+// ];
+
+const products = [];
+
+const product = {
+    id: 0,
+    name: "",
+    price: "", 
+}
